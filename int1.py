02a021ba03796ccff474ae70310995e082eb4b25
Risini Amarathunga <162025795+Rizz-33@users.noreply.github.com>
2024-06-11 18:07:23 +0530

    swap two numbers without a temp var

--- int1.py
+++ int1.py
@@ -1,10 +1,10 @@
 num1 = 10
 num2 = 20
 
-print ("Before", num1, num2)
+print ("Before swapping ", num1, num2)
 
 num1 = num1 + num2
 num2 = num1 - num2
 num1 = num1 - num2
 
-print ("After", num1, num2)
+print ("After swapping ", num1, num2)
