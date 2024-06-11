8cccd92186ed6e32b52181d3819502e756d04203
Risini Amarathunga <162025795+Rizz-33@users.noreply.github.com>
2024-06-11 19:03:49 +0530

    fibonacci series in recursive approach

---
+++ int4.py
@@ -0,0 +1,13 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        sequence = fibonacci(n - 1)
+        sequence.append(sequence[-1] + sequence[-2])
+        return sequence
+
+print(fibonacci(10))
